f7b670932cfcfa930cbffeb4eeb4415d9f6b9dd1
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-09 08:45:49 -0300

    feat: adiciona configuração de conexão com o MongoDB

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error("MONGO_URI não definido nas variáveis de ambiente.");
+}
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("Mongo DB conectado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao conectar ao MOngo DB: ", error);
+  }
+};
