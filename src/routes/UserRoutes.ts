ea231b0f3c3d4b2acfd53ba60147886b2034f9ea
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-09 18:15:22 -0300

    refactor: melhora a formatação e organiza as importações no UserRoutes

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import {UserController, validateSignupUser, validadeLoginUser} from "../controllers/UserController";
+import {
+  UserController,
+  validateSignupUser,
+  validadeLoginUser,
+} from "../controllers/UserController";
+import { authenticateJwt } from "../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/signup", validateSignupUser, UserController.signup);
-router.post("/login", validadeLoginUser, UserController.login)
+router.post("/login", validadeLoginUser, UserController.login);
+router.get("/me", authenticateJwt, UserController.getProfile);
 
 export default router;
