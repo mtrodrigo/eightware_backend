b6fdb00dfc42f766d216a54a97cdf746a8730fb5
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-09 17:15:27 -0300

    feat: adiciona rota de login no controlador de usuário

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,5 +4,6 @@ import {UserController, validateSignupUser} from "../controllers/UserController"
 const router = express.Router();
 
 router.post("/signup", validateSignupUser, UserController.signup);
+router.post("/login", UserController.login)
 
 export default router;
